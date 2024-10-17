d31501eaca130766c7fd0e50136a3b35bd6a00db
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-17 17:19:29 +0200

    email complete

--- dado/script.js
+++ dado/script.js
@@ -4,3 +4,7 @@
 //!TRACCIA
 //* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 //* Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//! RACCOLTA DATI
+//! ELABORAZIONE
+//! OUTPUT
--- mail/script.js
+++ mail/script.js
@@ -1,7 +1,51 @@
-//! TITOLO
+//# TITOLO
 //* Mail
 
-//!TRACCIA
+//# TRACCIA
 //* Chiedi all'utente la sua email,
 //* controlla che sia nella lista di chi può accedere,
 //* stampa un messaggio appropriato sull'esito del controllo.
+
+//# RACCOLTA DATI
+//* creare una lista di email che possono accedere
+const emails = [
+  "Marco@gmail.com",
+  "Francesco@gmail.com",
+  "Simone@gmail.com",
+  "Luca@gmail.com",
+  "Giovanni@gmail.com",
+];
+//* chiediamo all'utente di scrivere la sua email
+let emailUtente = prompt("Scrivi la tua email: ");
+
+//!VERSIONE SEMPLIFICATA
+//# ELABORAZIONE
+//* prendiamo in esame l'email ricevuta e controlliamo se è nella lista
+//? if (emails.includes(emailUtente)) {
+//   //# OUTPUT
+//?   alert("Puoi accedere");
+//? } else {
+//   //# OUTPUT
+//?   alert("Ci dispiace, non può accedere");
+//? }
+
+//!VERSIONE COMPLESSA
+//# ELABORAZIONE
+//*controlliamo con un ciclo che emailUtente sia nella lista
+let i = 0;
+let trovato = false;
+while (!trovato && i < emails.length) {
+  //* SE abbiamo un riscontro positivo, allora trasformiamo la variabile in true
+  if (emails[i] == emailUtente) {
+    trovato = true;
+  }
+  i++;
+}
+
+if (trovato) {
+  //# OUTPUT
+  alert("Puoi accedere");
+} else {
+  //# OUTPUT
+  alert("Ci dispiace, non può accedere");
+}
